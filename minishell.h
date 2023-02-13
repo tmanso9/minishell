07353be1a8c6e8ab9b39f4e644104b5666caa420
Afonso Morais Leitao Carvalho <amorais-@c2r5s2.42lisboa.com>
2023-02-13 11:41:05 +0000

    lib

---
+++ minishell.h
@@ -0,0 +1,19 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h> 
+#endif
